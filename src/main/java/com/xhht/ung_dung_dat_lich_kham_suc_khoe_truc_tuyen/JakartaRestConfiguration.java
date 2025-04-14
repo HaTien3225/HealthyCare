0b812596c444e290739eba55b1c82c289ee058eb
tofufuto <lehuy7804@gmail.com>
2025-04-14 12:38:45 +0700

    Initial commit

---
+++ src/main/java/com/xhht/ung_dung_dat_lich_kham_suc_khoe_truc_tuyen/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.xhht.ung_dung_dat_lich_kham_suc_khoe_truc_tuyen;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/xhht/ung_dung_dat_lich_kham_suc_khoe_truc_tuyen/resources/JakartaEE11Resource.java
@@ -0,0 +1,20 @@
+package com.xhht.ung_dung_dat_lich_kham_suc_khoe_truc_tuyen.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee11")
+public class JakartaEE11Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
